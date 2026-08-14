7cd984674e27eb39fcee8b426b6151b0d1524e90
JustPetrov <thepetrovshop@gmail.com>
2026-08-14 19:48:05 +0200

    Styles

--- pages/_app.js
+++ pages/_app.js
@@ -13,25 +13,29 @@ export default function App({ Component, pageProps }) {
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" 
         />
 
-        {/* Favicon Koppelingen (voor browsertabbladen) */}
+        {/* Favicon Koppelingen */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icon-192x192.png" />
         <link rel="icon" type="image/png" sizes="192x192" href="/icon-192x192.png" />
 
-        {/* Apple iOS Web App Instellingen (iPad / iPhone) */}
+        {/* Apple iOS Web App Instellingen */}
         <link rel="apple-touch-icon" href="/icon-192x192.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="BDM POS" />
 
-        {/* PWA Manifest & Thema Instellingen */}
+        {/* PWA Manifest & Thema */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
         <meta name="description" content="BENDEMEN Point of Sale Kassasysteem" />
       </Head>
 
-      {/* Globale Reset CSS voor een strakke kassa-layout zonder ongewenste marges */}
+      {/* Globale Reset & Tailwind CSS Directives */}
       <style jsx global>{`
+        @tailwind base;
+        @tailwind components;
+        @tailwind utilities;
+
         html,
         body {
           padding: 0;
@@ -40,7 +44,7 @@ export default function App({ Component, pageProps }) {
           color: #111111;
           font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
           -webkit-tap-highlight-color: transparent;
-          user-select: none; /* Voorkomt tekstselectie bij snel tikken op kassa-knoppen */
+          user-select: none;
         }
 
         *, *::before, *::after {
@@ -52,7 +56,6 @@ export default function App({ Component, pageProps }) {
         }
       `}</style>
 
-      {/* Renders de actieve pagina (zoals index.js of admin.js) */}
       <Component {...pageProps} />
     </>
   );
