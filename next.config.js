3590ed9313392a8a4361a90ff6321d71d4d3551c
JustPetrov <thepetrovshop@gmail.com>
2026-08-24 11:35:17 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,15 +3,29 @@ const withPWA = require('next-pwa')({
   disable: false,
   register: true,
   skipWaiting: true,
+  // Zorg dat offline verzoeken netjes worden opgevangen zonder vast te lopen
   fallbacks: {
-    // Voorkom dat hij vastloopt als er geen netwerk is bij een route-navigatie
-    document: '/login',
-  }
+    document: '/login', // Of je hoofdpagina
+  },
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Zorg dat assets correct worden opgeslagen voor offline gebruik
+  async headers() {
+    return [
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = withPWA(nextConfig);
