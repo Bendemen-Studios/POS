3817f2e24fe21bcc99e49a71ad1009dc7d849402
JustPetrov <thepetrovshop@gmail.com>
2026-08-25 13:30:38 +0200

    Fix PWA offline page and API caching

--- next.config.js
+++ next.config.js
@@ -3,9 +3,60 @@ const withPWA = require('next-pwa')({
   disable: false,
   register: true,
   skipWaiting: true,
+  reloadOnOnline: false,
+  cacheStartUrl: true,
+  dynamicStartUrl: false,
+  cacheOnFrontEndNav: true,
   fallbacks: {
     document: '/login',
   },
+  additionalManifestEntries: ['/login', '/select-store', '/'],
+  runtimeCaching: [
+    {
+      urlPattern: ({ request }) => request.destination === 'document',
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'bendemen-pos-pages',
+        networkTimeoutSeconds: 3,
+        expiration: {
+          maxEntries: 30,
+          maxAgeSeconds: 60 * 60 * 24 * 30,
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
+    },
+    {
+      urlPattern: ({ url, request }) =>
+        url.origin === self.location?.origin &&
+        url.pathname.startsWith('/api/') &&
+        request.method === 'GET',
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'bendemen-pos-api-get',
+        networkTimeoutSeconds: 3,
+        expiration: {
+          maxEntries: 100,
+          maxAgeSeconds: 60 * 60 * 24,
+        },
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
+    },
+    {
+      urlPattern: ({ request }) => request.destination === 'image',
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'bendemen-pos-images',
+        expiration: {
+          maxEntries: 200,
+          maxAgeSeconds: 60 * 60 * 24 * 30,
+        },
+      },
+    },
+  ],
 });
 
 /** @type {import('next').NextConfig} */
@@ -19,6 +70,10 @@ const nextConfig = {
           {
             key: 'Cache-Control',
             value: 'public, max-age=0, must-revalidate',
+          },
+          {
+            key: 'Content-Type',
+            value: 'application/javascript; charset=utf-8',
           },
         ],
       },
