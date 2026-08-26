5ad538098d5bb50ab327cfb0793ef262c135cfd8
JustPetrov <thepetrovshop@gmail.com>
2026-08-26 11:05:58 +0200

    Cache all customers with current points balance

--- pages/api/woocommerce/customers.js
+++ pages/api/woocommerce/customers.js
@@ -1,4 +1,5 @@
 import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
+import { extractCustomerPoints } from "../../../lib/customerPoints";
 
 const WooCommerce = WooCommerceRestApi.default || WooCommerceRestApi;
 
@@ -10,11 +11,38 @@ const api = new WooCommerce({
 });
 
 export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    res.setHeader('Allow', ['GET']);
+    return res.status(405).json({ success: false, error: `Method ${req.method} not allowed` });
+  }
+
   try {
-    const response = await api.get("customers", { per_page: 100 });
+    const allCustomers = [];
+    let page = 1;
+
+    while (true) {
+      const response = await api.get("customers", { per_page: 100, page });
+      const customers = Array.isArray(response.data) ? response.data : [];
+
+      allCustomers.push(...customers.map((customer) => ({
+        ...customer,
+        points_balance: extractCustomerPoints(customer),
+      })));
+
+      const totalPages = Number(
+        response.headers?.['x-wp-totalpages'] ||
+        response.headers?.['X-WP-TotalPages'] ||
+        0
+      );
+
+      if (customers.length < 100 || (totalPages > 0 && page >= totalPages)) break;
+      page += 1;
+    }
+
     return res.status(200).json({
       success: true,
-      customers: response.data || []
+      customers: allCustomers,
+      total: allCustomers.length,
     });
   } catch (error) {
     console.error("WooCommerce Customers Fetch Error:", error.message);
